b742ee526f933b0ebb57d955934b8ee884f59781
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2019-03-26 11:39:18 +0500

    Creating the views for admin flights

---
+++ resources/views/flights/add.blade.php
@@ -0,0 +1,127 @@
+@extends('layouts.master')
+
+@section('title', 'Admin Page - Add a Flight')
+
+@section('body')
+
+<div class="container">
+    @include('admin.includes.tabs')
+      <div class="row justify-content-center">
+          <div class="col-md-12">
+              <div class="card">
+                  <div class="card-header bg-white">{{ __('Add a Flight') }}</div>
+                    @if (session('status'))
+                      <div class="alert alert-success">
+                          {{ session('status') }}
+                      </div>
+                    @endif
+                  <div class="card-body">
+                    <form method="POST" action="{{ route('save_flight') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="from" class="col-md-4 col-form-label text-md-right">{{ __('From') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="from" type="text" class="form-control {{ $errors->has('from') ? ' is-invalid' : '' }}" name="from" value="{{ old('from') }}" placeholder="eg, Boston" required autofocus>
+
+                                @if ($errors->has('from'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('from') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="to" class="col-md-4 col-form-label text-md-right">{{ __('To') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="to" type="text" class="form-control {{ $errors->has('to') ? ' is-invalid' : '' }}" name="to" value="{{ old('to') }}" placeholder="eg, DC" required>
+
+                                @if ($errors->has('to'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('to') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="depart" class="col-md-4 col-form-label text-md-right">{{ __('Depart') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="depart" type="text" class="form-control {{ $errors->has('depart') ? ' is-invalid' : '' }}" name="depart" value="{{ old('depart') }}" required>
+
+                                @if ($errors->has('depart'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('depart') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="arrival" class="col-md-4 col-form-label text-md-right">{{ __('Arrival') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="arrival" type="text" class="form-control {{ $errors->has('arrival') ? ' is-invalid' : '' }}" name="arrival" value="{{ old('arrival') }}" required>
+
+                                @if ($errors->has('arrival'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('arrival') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="seats" class="col-md-4 col-form-label text-md-right">{{ __('Seats') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="seats" type="text" class="form-control {{ $errors->has('seats') ? ' is-invalid' : '' }}" name="seats" value="{{ old('seats') }}" placeholder="eg, 20" required>
+
+                                @if ($errors->has('seats'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('seats') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="price" class="col-md-4 col-form-label text-md-right">{{ __('Price') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="price" type="text" class="form-control {{ $errors->has('price') ? ' is-invalid' : '' }}" name="price" value="{{ old('price') }}" placeholder="eg, 600" required>
+
+                                @if ($errors->has('price'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('price') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                              <input type="hidden" name="created_by" value="{{ Auth::user()->id }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Save') }}
+                                </button>
+                            </div>
+                        </div>
+
+                    </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+</div>
+@include('admin.scripts.date_and_time')
+@endsection
---
+++ resources/views/flights/show.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.master')
+
+@section('title', 'Admin Page - Show Flights')
+
+@section('body')
+
+<div class="container">
+    @include('admin.includes.tabs')
+      <div class="row justify-content-center">
+          <div class="col-md-12">
+              <div class="card">
+                  <div class="card-header bg-white">{{ __('Show Flights') }}</div>
+                  <div class="card-body">
+                  @if( $flights->count() != 0)
+                    <table class="table table-hover">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">From</th>
+                        <th scope="col">To</th>
+                        <th scope="col">Depart</th>
+                        <th scope="col">Arrival</th>
+                        <th scope="col">Seats</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Created By</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @foreach ($flights as $flight)
+                      <tr>
+                        <th scope="row">{{$flight->id}}</th>
+                        <td>{{$flight->from}}</td>
+                        <td>{{$flight->to}}</td>
+                        <td>{{$flight->depart}}</td>
+                        <td>{{$flight->arrival}}</td>
+                        <td>{{$flight->seats}}</td>
+                        <td>{{$flight->price}}</td>
+                        <td>{{$flight->user->name}}</td>
+                        <td>
+                          <a href="{{route('edit_flight', [$flight->id])}}">
+                            <button type="button" class="btn btn-info">Edit</button>
+                          </a>
+                        </td>
+                        <td>
+                          <a href="{{route('delete_flight', [$flight->id])}}">
+                            <button type="button" class="btn btn-danger">Delete</button>
+                          </a>
+                        </td>
+                      </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                @else
+
+                  <div class="alert alert-info" role="alert">
+                    There are no flights to show, please add flights.
+                  </div>
+
+@endif
+                  </div>
+              </div>
+          </div>
+      </div>
+</div>
+@endsection
